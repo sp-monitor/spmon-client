9dbf40f0c21454dfd714a3fa2cc90ac3731f1567
Simon Carr <simon@soxprox.com>
2023-09-30 21:05:50 +0100

    suedo code for starting modules from the client

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	go startModules()
+	startUDPServer()
+}
+
+func startModules() {
+	modulePath := "./modules/spmon-module-cpu"
+    absPath, err := filepath.Abs(modulePath)
+    if err != nil {
+        fmt.Println("Error getting absolute path:", err)
+        return
+    }
+
+    cmd := exec.Command(absPath, "-h", "localhost", "-p", "9559")
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+
+    err = cmd.Run()
+    if err != nil {
+        fmt.Println("Error running executable:", err)
+        return
+    }
+}
+
+func startUDPServer() {
+	addr, err := net.ResolveUDPAddr("udp", "localhost:9559")
+	if err != nil {
+			fmt.Println("Error resolving UDP address:", err)
+			return
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+			fmt.Println("Error listening on UDP:", err)
+			return
+	}
+
+	defer conn.Close()
+
+	fmt.Println("UDP server listening on", addr)
+
+	for {
+			buf := make([]byte, 1024)
+			n, addr, err := conn.ReadFromUDP(buf)
+			if err != nil {
+					fmt.Println("Error reading from UDP:", err)
+					continue
+			}
+			fmt.Printf("Received %d bytes from %s: %s\n", n, addr, string(buf[:n]))
+	}
 }
